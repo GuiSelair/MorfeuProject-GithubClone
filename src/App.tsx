396096d7b9b9ab0431b083586f3d4b8c844942a8
guiselair <guilherme.lima1997@hotmail.com>
2020-09-03 21:31:59 -0300

    Add GlobalStyles, Create Componenets and Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
+import Header from './components/Header';
 
 const App: React.FC = () => {
   return (
-    <h1>
-      New Project
-      <span role="img" aria-label="rocket">
-        🚀
-      </span>
-    </h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { FaGithub } from 'react-icons/fa';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  background: var(--header);
+  padding: 11px 16px;
+`;
+
+export const GithubLogo = styled(FaGithub)`
+  fill: var(--logo);
+  width: 32px;
+  flex-shrink: 0;
+  height: 32px;
+`;
+export const SearchForm = styled.form`
+  padding-left: 16px;
+  width: 100%;
+
+  input {
+    background: var(--search);
+    border-radius: 6px;
+    padding: 7px 12px;
+    width: 100%;
+    transition: width 0.2s ease-out, color 0.2s ease-out;
+
+    &:focus {
+      width: 318px;
+    }
+  }
+`;
