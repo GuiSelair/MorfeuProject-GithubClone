1b05b2a3b67a3066baaeb68076fbdd0c7db849bd
Guilherme Selair <guilherme.lima1997@hotmail.com>
2022-08-11 01:28:17 -0300

    Fix: Build

---
+++ src/@types/react-router-dom/index.d.ts
@@ -0,0 +1,28 @@
+import { match } from 'react-router';
+import * as React from 'react';
+import * as H from 'history';
+export { History } from 'history';   // ADD THIS LINE ?
+
+export {
+    generatePath,
+    Prompt,
+    MemoryRouter,
+    RedirectProps,
+    Redirect,
+    RouteChildrenProps,
+    RouteComponentProps,
+    RouteProps,
+    Route,
+    Router,
+    StaticRouter,
+    SwitchProps,
+    Switch,
+    match,
+    matchPath,
+    withRouter,
+    RouterChildContext,
+    useHistory,
+    useLocation,
+    useParams,
+    useRouteMatch,
+} from 'react-router';
